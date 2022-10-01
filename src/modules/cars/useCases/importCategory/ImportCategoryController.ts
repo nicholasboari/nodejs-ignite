982e4f5df7e6799ea75a06bbf8038469d103d428
Nicholas <nicholasboari@gmail.com>
2022-09-30 23:44:47 -0300

    adding upload and api-docs

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,11 @@
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+import { Request, Response } from "express";
+
+export class ImportCategoryController {
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+  handle(request: Request, response: Response) {
+    const { file } = request;
+    this.importCategoryUseCase.execute(file);
+    return response.send();
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+import { router } from "./routes/index";
 
 const app = express();
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
-app.use("/specification", specificationsRoutes);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
 app.listen(3333, () => console.log("Server is running"));
